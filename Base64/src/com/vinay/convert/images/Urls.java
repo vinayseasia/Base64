49e883e7d21aa8fe5c8d95134b4d3e63add30280
vinay <vinay.contact01@gmail.com>
2013-12-11 01:42:39 +0530

    added URLS

---
+++ Base64/src/com/vinay/convert/images/Urls.java
@@ -0,0 +1,22 @@
+package com.cci.hud.deal.utils;
+
+public class Urls {
+
+	public static String BASE_URL = "http://homedeal4u.com/";
+	
+	public static String COUNTRY_URL = BASE_URL + "iphone_hd4u/property.php";
+	public static String IMAGES_BASE_URL = BASE_URL + "images/rentalimages/";
+	public static String PDF_URL= BASE_URL + "condition_reports/";
+	public static String PDF_URL1= BASE_URL + "apraisel_reports/";
+	public static String LOGIN_URL = COUNTRY_URL;
+	public static String SIGNUP_URL = COUNTRY_URL;
+	public static String SIGNUP_URL_FB = "http://shinedezigninfotech.com/iphone_hd4u/property.php";
+	
+	
+	// (OLD URL)
+	//public static String COUNTRY_URL ="http://ccihud.com/appfiles/ccihudapi.php";
+	//public static String IMAGES_BASE_URL = "http://ccihud.com/images/rentalimages/";
+	//public static String COUNTRY_URL ="http://ccihud.com/iphone_hd4u/property.php";
+	//public static String COUNTRY_URL ="http://shinedezigninfotech.com/iphone_hd4u/property.php";         
+
+}
